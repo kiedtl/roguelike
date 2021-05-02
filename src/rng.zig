21591e0569eac8dcbe9262c2a188af61d66d141f
Kiëd Llaentenn <kiedtl@tilde.team>
2021-05-02 05:10:39 +0000

    stage1(05-01): drunken walk mapgen

---
+++ src/rng.zig
@@ -0,0 +1,37 @@
+// Yes, yes, I know Zig has a `rand` module. But, hey, reinventing the wheel is
+// fun!!
+
+const std = @import("std");
+const rand = std.rand;
+const math = std.math;
+
+var rng: rand.Isaac64 = undefined;
+
+pub fn init() void {
+    rng = rand.Isaac64.init(0xeffaced_facade);
+}
+
+pub fn boolean() bool {
+    return rng.random.int(u1) == 1;
+}
+
+pub fn shuffle(comptime T: type, arr: []T) void {
+    rng.random.shuffle(T, arr);
+}
+
+pub fn choose(comptime T: type, arr: []const T, weights: []const usize) !T {
+    if (arr.len != weights.len) return error.InvalidWeights;
+
+    var weight_total: usize = 0;
+    var selected = arr[0];
+
+    for (arr) |item, index| {
+        const weight = weights[index];
+        const rnd = rng.random.int(usize) % (weight_total + weight);
+        if (rnd >= weight_total) // probability is weight/(total+weight)
+            selected = item;
+        weight_total += weight;
+    }
+
+    return selected;
+}
