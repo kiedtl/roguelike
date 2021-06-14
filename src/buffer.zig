b8f27692ac2a8a58346c405654e123ad5e900d43
Kiëd Llaentenn <kiedtl@tilde.team>
2021-06-14 16:10:32 +0000

    Add orderedRemove() and pop() to StackBuffer

--- src/buffer.zig
+++ src/buffer.zig
@@ -8,6 +8,7 @@ pub fn StackBuffer(comptime T: type, comptime capacity: usize) type {
     return struct {
         data: [capacity]T = undefined,
         len: usize = 0,
+        capacity: usize = capacity,
 
         const Self = @This();
 
@@ -23,6 +24,29 @@ pub fn StackBuffer(comptime T: type, comptime capacity: usize) type {
 
         pub fn constSlice(self: *const Self) []const T {
             return self.data[0..self.len];
+        }
+
+        pub fn orderedRemove(self: *Self, index: usize) !T {
+            if (self.len == 0 or index >= self.len) {
+                return error.IndexOutOfRange;
+            }
+
+            const newlen = self.len - 1;
+            if (newlen == index) {
+                return try self.pop();
+            }
+
+            const item = self.data[index];
+            for (self.data[index..newlen]) |*data, j|
+                data.* = self.data[index + 1 + j];
+            self.len = newlen;
+            return item;
+        }
+
+        pub fn pop(self: *Self) !T {
+            if (self.len == 0) return error.IndexOutOfRange;
+            self.len -= 1;
+            return self.data[self.len];
         }
 
         pub fn append(self: *Self, item: T) !void {
