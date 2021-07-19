7a644538fd67e9ffdec8f09bfb6ce8bfcfd96ac8
Kiëd Llaentenn <kiedtl@tilde.team>
2021-07-19 12:07:08 +0000

    Put together PRI_power and PRI_cleaning

--- src/mapgen.zig
+++ src/mapgen.zig
@@ -1442,7 +1442,6 @@ pub const Configs = [LEVELS]LevelConfig{
         .prefabs = LevelConfig.RPBuf.init(&[_][]const u8{
             "PRI_start",
             "PRI_power",
-            "PRI_cleaning",
         }),
         .distances = [2][10]usize{
             .{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
@@ -1458,7 +1457,6 @@ pub const Configs = [LEVELS]LevelConfig{
             "PRI_start",
             "PRI_power",
             "PRI_insurgency",
-            "PRI_cleaning",
         }),
         .distances = [2][10]usize{
             .{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
