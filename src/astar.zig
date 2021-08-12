e80254f989096142d870cfc7c3b2ca2bb9cd5336
Kiëd Llaentenn <kiedtl@tilde.team>
2021-08-12 15:52:10 +0000

    Optimize A* (thanks /u/aotdev)

--- src/astar.zig
+++ src/astar.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const mem = std.mem;
+const testing = std.testing;
 const math = std.math;
 const assert = std.debug.assert;
 
@@ -8,14 +9,13 @@ const state = @import("state.zig");
 
 var cache: std.AutoHashMap(Path, Direction) = undefined;
 
-const NodeState = enum { Open, Closed };
+const NodeState = enum { Open, Closed, Unexplored };
 
 const Node = struct {
-    coord: Coord,
-    parent: ?*Node,
-    g: usize,
-    h: usize,
-    state: NodeState,
+    parent: ?*Node = null,
+    g: usize = 0,
+    h: usize = 0,
+    state: NodeState = .Unexplored,
 
     pub inline fn f(n: *const Node) usize {
         return n.g + n.h;
@@ -28,6 +28,17 @@ const Node = struct {
 
 const NodeArrayList = std.ArrayList(Node);
 const NodePriorityQueue = std.PriorityQueue(*Node);
+
+// Do some pointer stuff to extract x/y coordinates from a node's pointer
+// location relative to the matrix start
+//
+// Thanks to /u/aotdev for pointing this trick out to me
+inline fn coordFromPtr(node: *Node, matrix_start: *Node, z: usize) Coord {
+    const off = (@ptrToInt(node) - @ptrToInt(matrix_start)) / @sizeOf(Node);
+    const x = off % WIDTH;
+    const y = off / WIDTH;
+    return Coord.new2(z, x, y);
+}
 
 // Manhattan: d = dx + dy
 inline fn manhattanHeuristic(a: Coord, b: Coord) usize {
@@ -67,29 +78,30 @@ pub fn path(
     }
 
     var open_list = NodePriorityQueue.init(alloc, Node.betterThan);
-    var nodes: [HEIGHT][WIDTH]?Node = [_][WIDTH]?Node{[_]?Node{null} ** WIDTH} ** HEIGHT;
+    var nodes: [HEIGHT][WIDTH]Node = [_][WIDTH]Node{[_]Node{.{}} ** WIDTH} ** HEIGHT;
 
     nodes[start.y][start.x] = Node{
-        .coord = start,
         .g = 0,
         .h = manhattanHeuristic(start, goal),
         .parent = null,
         .state = .Open,
     };
-    open_list.add(&nodes[start.y][start.x].?) catch unreachable;
+    open_list.add(&nodes[start.y][start.x]) catch unreachable;
 
     while (open_list.count() > 0) {
         var current_node: *Node = open_list.remove();
+        const cur_coord = coordFromPtr(current_node, &nodes[0][0], start.z);
 
-        if (current_node.coord.eq(goal)) {
+        if (cur_coord.eq(goal)) {
             open_list.deinit();
 
             var list = CoordArrayList.init(alloc);
-            var current = current_node.*;
+            var current = current_node;
             while (true) {
-                list.append(current.coord) catch unreachable;
+                const coord = coordFromPtr(current, &nodes[0][0], start.z);
+                list.append(coord) catch unreachable;
                 if (current.parent) |parent| {
-                    current = parent.*;
+                    current = parent;
                 } else break;
             }
 
@@ -101,10 +113,9 @@ pub fn path(
 
         const neighbors = DIRECTIONS;
         neighbor: for (neighbors) |neighbor| {
-            if (current_node.coord.move(neighbor, state.mapgeometry)) |coord| {
-                if (nodes[coord.y][coord.x]) |*other_node|
-                    if (other_node.state == .Closed)
-                        continue;
+            if (cur_coord.move(neighbor, state.mapgeometry)) |coord| {
+                if (nodes[coord.y][coord.x].state == .Closed)
+                    continue;
 
                 if (!is_walkable(coord, opts) and !goal.eq(coord)) continue;
 
@@ -112,25 +123,42 @@ pub fn path(
                     pathfindingPenalty(coord, opts);
                 const new_g = current_node.g + cost;
 
-                if (nodes[coord.y][coord.x]) |*other_node|
-                    if (other_node.g < new_g)
-                        continue;
+                if (nodes[coord.y][coord.x].state == .Open and nodes[coord.y][coord.x].g < new_g)
+                    continue;
 
                 const node = Node{
-                    .coord = coord,
                     .parent = current_node,
                     .g = new_g,
                     .h = manhattanHeuristic(coord, goal),
                     .state = .Open,
                 };
 
-                const in_ol = if (nodes[coord.y][coord.x]) |*on| on.state == .Open else false;
+                const old_state = nodes[coord.y][coord.x].state;
                 nodes[coord.y][coord.x] = node;
-                if (!in_ol) open_list.add(&nodes[coord.y][coord.x].?) catch unreachable;
+                if (old_state != .Open) // not in open list already
+                    open_list.add(&nodes[coord.y][coord.x]) catch unreachable;
             }
         }
     }
 
     open_list.deinit();
     return null;
+}
+
+test "coordFromPtr" {
+    var nodes: [HEIGHT][WIDTH]Node = [_][WIDTH]Node{[_]Node{.{}} ** WIDTH} ** HEIGHT;
+    const begin = &nodes[0][0];
+    const z = 0;
+
+    const cases = [_]Coord{
+        Coord.new2(z, 0, 0),
+        Coord.new2(z, 1, 1),
+        Coord.new2(z, 5, 9),
+        Coord.new2(z, 15, 20),
+    };
+
+    for (&cases) |expected| {
+        const got = coordFromPtr(&nodes[expected.y][expected.x], begin, z);
+        testing.expectEqual(expected, got);
+    }
 }
