83b1e6f0340588d37695732e436085a5cb9c9d25
Kiëd Llaentenn <kiedtl@tilde.team>
2022-04-29 00:03:47 +0000

    feat: give -melee nbonus for .Fear

--- src/combat.zig
+++ src/combat.zig
@@ -21,6 +21,7 @@ pub const CHANCE_FOR_DIP_EFFECT = 33;
 
 const ATTACKER_ENRAGED_BONUS: isize = 20;
 const ATTACKER_OPENMELEE_BONUS: isize = 10;
+const ATTACKER_FEAR_NBONUS: isize = 10;
 const ATTACKER_HELD_NBONUS: isize = 20;
 const ATTACKER_STUN_NBONUS: isize = 10;
 
@@ -66,6 +67,7 @@ pub fn chanceOfMeleeLanding(attacker: *const Mob, defender: ?*const Mob) usize {
     chance += if (attacker.isUnderStatus(.Enraged) != null) ATTACKER_ENRAGED_BONUS else 0;
     chance += if (attacker.isUnderStatus(.OpenMelee) != null and nearby_walls <= 3) ATTACKER_OPENMELEE_BONUS else 0;
 
+    chance -= if (attacker.isUnderStatus(.Fear)) |_| ATTACKER_FEAR_NBONUS else 0;
     chance -= if (attacker.isUnderStatus(.Held)) |_| ATTACKER_HELD_NBONUS else 0;
     chance -= if (attacker.isUnderStatus(.Stun)) |_| ATTACKER_STUN_NBONUS else 0;
 
