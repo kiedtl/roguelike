b575f67a0b43c59063a82f6cdafebf3989ee04cd
Kiëd Llaentenn <kiedtl@tilde.team>
2022-09-16 20:13:06 -0400

    feat: (WIP) add sprites for lab wall + lab/pri windows (SDL)

--- src/sprites.zig
+++ src/sprites.zig
@@ -7,6 +7,8 @@
 pub const Sprite = enum(u21) {
     S_G_Wall_Finished = 0x2600,
     S_G_Wall_Rough = 0x2601,
+    S_G_Wall_Scifish = 0x2602,
+    S_G_Wall_Window = 0x2603,
 
     S_G_T_Metal = 0x2610,
 };
