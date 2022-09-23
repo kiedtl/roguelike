d8bcfeebdf07ae5da763409271d2e3dee2ce26ec
Kiëd Llaentenn <kiedtl@tilde.team>
2022-09-23 18:28:51 -0400

    feat: make color of copper more washed-out

--- src/surfaces.zig
+++ src/surfaces.zig
@@ -150,7 +150,7 @@ pub const MetalTerrain = Terrain{
 pub const CopperTerrain = Terrain{
     .id = "t_copper",
     .name = "copper",
-    .color = 0x783724, // copperish red
+    .color = 0x998883,
     .tile = ':',
     .resists = .{ .rElec = -25 },
     .effects = &[_]StatusDataInfo{
