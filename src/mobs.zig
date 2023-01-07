ba233b0bb9d6e2ed2382fb9300eb5eb9008583d8
Kiëd Llaentenn <kiedtl@tilde.team>
2023-01-07 14:12:00 -0500

    tweak: decrease @ HP (20 -> 14)

--- src/mobs.zig
+++ src/mobs.zig
@@ -552,7 +552,7 @@ pub const PlayerTemplate = MobTemplate{
         .deg360_vision = true,
         .no_show_fov = true,
 
-        .max_HP = 20,
+        .max_HP = 14,
         .memory_duration = 10,
 
         .stats = .{ .Willpower = 4, .Missile = 60, .Evade = 10, .Vision = 12, .Sneak = 4 },
